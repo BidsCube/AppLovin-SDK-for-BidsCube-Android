6744f79884673c1fa25c5368d604321396c07d1a
catchman-ua <maksim.boiko.ua@gmail.com>
2026-03-12 08:13:39 +0200

    Refactoring action logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+// Set AndroidX before any project build script runs (CI often has no gradle.properties)
+gradle.beforeProject { proj ->
+    if (!proj.hasProperty("android.useAndroidX")) {
+        proj.ext.set("android.useAndroidX", true)
+    }
+}
+
 plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
@@ -10,10 +17,6 @@ allprojects {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
-    }
-    // Required for AndroidX deps; CI may not have gradle.properties
-    if (!project.hasProperty("android.useAndroidX")) {
-        project.ext.set("android.useAndroidX", true)
     }
 }
 
